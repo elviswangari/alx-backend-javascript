59b620be180226d1fc04de70cbe5a97968ca0acf
elviswangari <elviswangare@gmail.com>
2023-06-28 20:37:17 +0000

    promise resolve and reject

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
+    if (success) {
       resolve({ status: 200, body: 'success' });
     } else {
       reject(new Error('The fake API is not working currently'));
