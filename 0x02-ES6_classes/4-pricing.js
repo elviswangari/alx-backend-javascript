fcf8583853e7d879dc8ac4886f880cf7681685db
elviswangari <elviswangare@gmail.com>
2023-06-30 01:31:00 +0300

    static methods

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(val) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = val;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(val) {
+    if (!(val instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._currency = val;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} ${this.currency.code}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' && typeof conversionRate !== 'number') {
+      throw new TypeError('must be a number');
+    }
+
+    return amount * conversionRate;
+  }
+}
