184aeaa416220ff50aa4e3b0ce41cdf7dfd261ea
elviswangari <elviswangare@gmail.com>
2023-07-05 03:49:45 +0300

    combination

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrade) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const gradeObj = newGrade.find((grade) => grade.studentId === student.id);
       const grade = gradeObj ? gradeObj.grade : 'N/A';
       return { ...student, grade };
     });
