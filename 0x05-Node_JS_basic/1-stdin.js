44befe29748424cf42c8353dc4c1ece306574e41
elviswangari <elviswangare@gmail.com>
2023-08-21 21:51:35 +0300

    process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+process.stdin.setEncoding('utf8');
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (name) => {
+  console.log('Your name is:', name.trim());
+  console.log('This important software is now closing');
+  process.exit();
+});
